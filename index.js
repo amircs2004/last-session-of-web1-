b26da256630d7cf154cd7a21694bb4e0343fec7e
amircs2004 <saidimahdiamir@gmail.com>
2024-09-06 17:30:40 +0100

    add html and css and js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-let cels = 7
-function celsToFahrn() {
-    let fahrn 
-    return 
+let convertEL = document.querySelector("#convert")
+let temperatureEL = document.getElementById("temperature")
+let resultEL = document.getElementById("result")
+let outputEL = document.getElementById("output")
+
+console.log(resultEL);
+
+resultEL.textContent = ""
+
+convert()
+function convert() {
+
+    let f = temperatureEL.value
+    let cTf=  (f * 9/5) + 32
+    resultEL.innerText = cTf
+}
+convertEL.addEventListener("submit" , convert())
+
+if (temperatureEL === "") {
+    resultEL.innerText = ""
 }
